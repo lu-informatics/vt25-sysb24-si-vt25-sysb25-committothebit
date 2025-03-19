27b796e0c063c6347bd282b278e739cdc4c2ac5f
erikhellryd <erik.hellryd@gmail.com>
2025-03-19 10:20:05 +0100

    Added button on MagicRecipeGeneratorPage and placeholer method in the code-behind.

--- Pages/MagicRecipeGeneratorPage.xaml.cs
+++ Pages/MagicRecipeGeneratorPage.xaml.cs
@@ -6,4 +6,24 @@ public partial class MagicRecipeGeneratorPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void GenerateRecipeButton_Clicked(object sender, EventArgs e)
+	{
+		// Logic to generate a random recipe
+		string[] recipes = new string[]
+		{
+			"Spaghetti Bolognese",
+			"Chicken Alfredo",
+			"Vegetable Stir Fry",
+			"Beef Tacos",
+			"Grilled Salmon"
+		};
+
+		Random random = new Random();
+		int index = random.Next(recipes.Length);
+		string selectedRecipe = recipes[index];
+
+		// Update the label with the generated recipe
+		RecipeLabel.Text = $"Your random recipe is: {selectedRecipe}";
+	}
 }
